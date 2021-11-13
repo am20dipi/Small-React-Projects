6a2e18aa91a2977fa0f2083f14f390ec4919a734
Adriana <am20dipi@alum.siena.edu>
2021-11-12 19:38:17 -0500

    Using useState to set width of component to 0%; creating openSideNav and closeSideNav with onCLick functionality

--- sidenav/src/App.js
+++ sidenav/src/App.js
@@ -1,11 +1,27 @@
 import SideNav from './SideNav';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [width, setWidth] = useState('0%')
+
+  const openSideNav = () => {
+    setWidth('25%')
+  }
+
+  const closeSideNav = () => {
+    setWidth('0%')
+}
+
   return (
     <div className="App">
+      <div className="welcome">
         <h2>WELCOME</h2>
-        <SideNav></SideNav>
+        <button onClick={openSideNav}>Open</button><br/>
+      </div>
+      <div className="sidebar">
+        <SideNav width={width} closeSideNav={closeSideNav} /><br/>
+      </div>
     </div>
   )
 }
--- sidenav/src/SideNav.js
+++ sidenav/src/SideNav.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const SideNav = (props) => {
+    
+    
     return (
         <div className="sidenav">
-            <a href="#section">About</a>
-            <a href="#section">Contact Us</a>
-            <a href="#section">FAQ</a>
-            <a href="#section">Home</a>
+            <button onClick={props.closeSideNav}>X</button><br/>
+            <a href="#section" style={{width: props.width}}>About</a><br/>
+            <a href="#section" style={{width: props.width}}>Contact Us</a><br/>
+            <a href="#section" style={{width: props.width}}>FAQ</a><br/>
+            <a href="#section" style={{width: props.width}}>Home</a><br/>
         </div>
     )
 }
