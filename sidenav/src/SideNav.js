fb0db113cd2bc204e48070966d5e4ff3e0978ab1
Adriana <am20dipi@alum.siena.edu>
2021-11-12 19:21:58 -0500

    Removing auto generated stuff from App.js; creating SideNav component with some ahrefs to different links; importing SideNav into App.js

---
+++ sidenav/src/SideNav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SideNav = (props) => {
+    return (
+        <div className="sidenav">
+            <a href="#section">About</a>
+            <a href="#section">Contact Us</a>
+            <a href="#section">FAQ</a>
+            <a href="#section">Home</a>
+        </div>
+    )
+}
+
+export default SideNav
