cca65f2d9c84006b3cd7ccbccdf235fe88b8e63b
Adriana <am20dipi@alum.siena.edu>
2021-11-15 11:58:30 -0500

    installing np react-hookform; creating form element in Contact.js; importing Contact.js in app.js and rendering the component

--- email_form/src/App.js
+++ email_form/src/App.js
@@ -1,10 +1,10 @@
-
+import { Contact } from './Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Contact/>
     </div>
   );
 }
--- email_form/src/Contact.js
+++ email_form/src/Contact.js
@@ -4,6 +4,15 @@ export const Contact = () => {
     return (
         <div className="contact">
             <h2>CONTACT</h2>
+            <form id='contact-form'>
+                <input type='text' name='username' placeholder='Username'/>
+                <br/>
+                <input type='email' name='email' placeholder='Email'/>
+                <br/>
+                <textarea name='message' placeholder='Write your message here...'/>
+                <br/>
+                <input type='submit' value='Submit' />
+            </form>
         </div>
     )
 }
