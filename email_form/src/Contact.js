66b92aa8de6b474601707a38a7f34aad622d1131
Adriana <am20dipi@alum.siena.edu>
2021-11-15 12:28:18 -0500

    styling in css; trying out refs + aria-invalid; had to remove bc of bug; finishing buildingout form element

--- email_form/src/Contact.js
+++ email_form/src/Contact.js
@@ -1,18 +1,32 @@
 import React from 'react'
+//import { useForm } from 'react-hook-form'
 
 export const Contact = () => {
+   // const { register, handleSubmit, errors } = useForm()
+    
+    
+    const handleSubmit = (data) => {
+        console.log(data)
+    }
     return (
         <div className="contact">
             <h2>CONTACT</h2>
-            <form id='contact-form'>
-                <input type='text' name='username' placeholder='Username'/>
-                <br/>
-                <input type='email' name='email' placeholder='Email'/>
-                <br/>
-                <textarea name='message' placeholder='Write your message here...'/>
-                <br/>
-                <input type='submit' value='Submit' />
+            <form id='contact-form' onSubmit={handleSubmit}>
+                <input
+                type='text'
+                name='username'
+                placeholder='name' 
+                maxLength='30'
+                />
+                <br/><br/>
+                <input type='email' name='email' maxLength='30' placeholder='email'/>
+                <br/><br/>
+                <textarea name='message' maxLength='1000' placeholder='write your message here...'/>
+                <br/><br/>
+                <input id='submit' type='submit' value='submit' />
             </form>
         </div>
     )
 }
+
+export default Contact
