8cbb0155b0a0a3038e199f01ed308e1f7f21e2e5
Adriana <am20dipi@alum.siena.edu>
2021-12-07 14:31:25 -0500

    Creating questions and answer options; each answerOption has an isCorrect property pointing to a boolean

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
+
+  const questions = [
+    {
+      questionText: 'How Many Countries Are There In The World?',
+      answerOptions: [
+        { answerText: '120', isCorrect: false},
+        { answerText: '183', isCorrect: false},
+        { answerText: '170', isCorrect: false},
+        { answerText: '195', isCorrect: true}
+      ],
+    },
+    {
+      questionText: 'What Is The "G" in ROY G. BIV?',
+      answerOptions: [
+        { answerText: 'Gray', isCorrect: false},
+        { answerText: 'Green', isCorrect: true},
+        { answerText: 'Grey', isCorrect: false},
+        { answerText: 'Gristle', isCorrect: false}
+      ],
+    },
+    {
+      questionText: 'What Is The Breed Of Cat?',
+      answerOptions: [
+        { answerText: 'Siberian Short-Snout', isCorrect: false},
+        { answerText: 'Laos Lion', isCorrect: false},
+        { answerText: 'Michigan Bengal', isCorrect: false},
+        { answerText: 'Norwegian Forest Cat', isCorrect: true}
+      ],
+    },
+    {
+      questionText: 'What Continent Is Suriname Located On?',
+      answerOptions: [
+        { answerText: 'South America', isCorrect: true},
+        { answerText: 'Asia', isCorrect: false},
+        { answerText: 'Europe', isCorrect: false},
+        { answerText: 'North America', isCorrect: false}
+      ],
+    },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
-  );
+  )
 }
 
 export default App;
